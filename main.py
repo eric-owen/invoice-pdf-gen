4b882f302c7f8dd7ef7a7f0432610dddfff6804f
eric-owen <ejo.owen@icloud.com>
2023-03-24 10:17:44 -0400

    refactors by adding seperate function w/ docstring, checks main,

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
-filepaths = glob.glob("invoices/*.xlsx")
 
-for filepath in filepaths:
+def create_invoice_pdf(filepath):
     pdf = FPDF(orientation="P", unit="mm", format="A4")
 
     pdf.add_page()
@@ -15,31 +14,36 @@ for filepath in filepaths:
 
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"Invoice# {inv_num}", ln=1)
-
-    pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice# {inv_date} ", ln=1)
+    pdf.cell(w=50, h=8, txt=f"Invoice Date: {inv_date} ", ln=1)
 
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
-    columns = df.columns
-    columns = [item.replace('_', ' ').title() for item in columns]
+    # Convert column names to title case
+    columns = df.columns.str.replace('_', ' ').str.title()
 
     total_sum = df['total_price'].sum()
-    pdf.cell(w=30, h=8, txt='Invoice Total: ' + str(total_sum), ln=1)
+    pdf.cell(w=30, h=8, txt=f'Invoice Total: {total_sum:.2f}', ln=1)
 
+    # Print column headers
     pdf.set_font(family="Times", size=10, style='B')
-    pdf.cell(w=30, h=8, txt=columns[0], border=1)
-    pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=30, h=8, txt=columns[2], border=1)
-    pdf.cell(w=30, h=8, txt=columns[3], border=1)
-    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+    for col in columns:
+        pdf.cell(w=30, h=8, txt=col, border=1)
+    pdf.ln()
 
-    for index, row in df.iterrows():
-        pdf.set_font(family="Times", size=10)
+    # Print rows
+    pdf.set_font(family="Times", size=10)
+    for _, row in df.iterrows():
         pdf.cell(w=30, h=8, txt=str(row['product_id']), border=1)
         pdf.cell(w=70, h=8, txt=str(row['product_name']), border=1)
         pdf.cell(w=30, h=8, txt=str(row['amount_purchased']), border=1)
-        pdf.cell(w=30, h=8, txt=str(row['price_per_unit']), border=1)
-        pdf.cell(w=30, h=8, txt=str(row['total_price']), border=1, ln=1)
+        pdf.cell(w=30, h=8, txt=f'{row["price_per_unit"]:.2f}', border=1)
+        pdf.cell(w=30, h=8, txt=f'{row["total_price"]:.2f}', border=1, ln=1)
 
     pdf.output(f"PDFs/{inv_num}.pdf")
+
+
+if __name__ == "__main__":
+    filepaths = glob.glob("invoices/*.xlsx")
+
+    for filepath in filepaths:
+        create_invoice_pdf(filepath)
